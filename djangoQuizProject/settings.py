a79141b493d95a0fb67dfcbba179973bc2dcc5ce
Hanae123458 <hanae.bouichnad@gmail.com>
2025-04-23 12:32:10 +0100

    resultatQuiz

--- djangoQuizProject/settings.py
+++ djangoQuizProject/settings.py
@@ -89,16 +89,16 @@ DATABASES = {
 }
 
 
-#DATABASES={
-#    'default':{ 
-#                'ENGINE': 'django.db.backends.mysql',
-#                'NAME': 'gestionquiz',
-#                'USER': 'root',
-#                'PASSWORD': '',
-#                'HOST': 'localhost',
-#                'PORT': '3306',
-#            }
-#}
+DATABASES={
+    'default':{ 
+                'ENGINE': 'django.db.backends.mysql',
+                'NAME': 'gestionquiz',
+                'USER': 'root',
+                'PASSWORD': '',
+                'HOST': 'localhost',
+                'PORT': '3306',
+            }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
